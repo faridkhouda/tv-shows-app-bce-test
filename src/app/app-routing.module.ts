02403a470588658ddcc9eb1502ab7c1264c16fae
faridkhouda <geekamazighe@gmail.com>
2023-12-08 09:12:57 +0100

    Restructure project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ShowListComponent } from './features/show-list/show-list.component';
+import { ShowDetailsComponent } from './features/show-details/show-details.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'shows', component: ShowListComponent },
+  { path: 'show/:id', component: ShowDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { ShowListComponent } from './features/show-list/show-list.component';
+import { ShowDetailsComponent } from './features/show-details/show-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShowListComponent,
+    ShowDetailsComponent
   ],
   imports: [
     BrowserModule,
